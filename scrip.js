f7c5ca554c6951c233727896f5c5fc7e02932dde
Silvia Daniela Delgado <128757903+silvia-daniela-delgado@users.noreply.github.com>
2023-09-29 16:29:08 -0300

    Comentarios en codigo

--- scrip.js
+++ scrip.js
@@ -1,44 +1,52 @@
-const inputTexto = document.querySelector(".input-texto")
+// Obtener el elemento de entrada de texto y el elemento de mensaje
+const inputTexto = document.querySelector(".input-texto");
+const mensaje = document.querySelector(".mensaje");
 
-const mensaje = document.querySelector(".mensaje")
-
-function btnEncriptar(){
-    const encriptado = encriptar(inputTexto.value); 
+// Función para encriptar el texto
+function btnEncriptar() {
+    const encriptado = encriptar(inputTexto.value);
     mensaje.value = encriptado;
-    mensaje.style.backgroundImage = "none"; 
-    inputTexto.value = "" 
+    mensaje.style.backgroundImage = "none"; // Elimina la imagen de fondo
+    inputTexto.value = ""; // Limpia el campo de entrada de texto
 }
-function encriptar(textoEncriptado) {
-    let codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
-    textoEncriptado = textoEncriptado.toLowerCase(); /* pasar el texto a minúscula */ 
 
-    for (let i = 0; i < codigo.length; i++){
-        if (textoEncriptado.includes(codigo[i][0])){
-            textoEncriptado = textoEncriptado.replaceAll(codigo[i][0], codigo[i][1]) 
-            
-        } 
+function encriptar(textoEncriptado) {
+    // Definir un código de reemplazo
+    let codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    textoEncriptado = textoEncriptado.toLowerCase(); // Convertir el texto a minúsculas
+
+    for (let i = 0; i < codigo.length; i++) {
+        if (textoEncriptado.includes(codigo[i][0])) {
+            textoEncriptado = textoEncriptado.replaceAll(codigo[i][0], codigo[i][1]);
+        }
     }
     return textoEncriptado;
 }
+
+// Función para desencriptar el texto
 function btnDesencriptar() {
-    const textoEncriptado = desencriptar(inputTexto.value)
-    mensaje.value = textoEncriptado
-    inputTexto.value = ""  
+    const textoEncriptado = desencriptar(inputTexto.value);
+    mensaje.value = textoEncriptado;
+    inputTexto.value = "";
 }
+
 function desencriptar(textoDesencriptado) {
-    let codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
+    // Definir el mismo código de reemplazo utilizado para la encriptación
+    let codigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     textoDesencriptado = textoDesencriptado.toLowerCase();
 
-    for(let i=0; i < codigo.length; i++) {
-        if(textoDesencriptado.includes(codigo[i][1])) {
-            textoDesencriptado = textoDesencriptado.replaceAll(codigo[i][1], codigo[i][0])
+    for (let i = 0; i < codigo.length; i++) {
+        if (textoDesencriptado.includes(codigo[i][1])) {
+            textoDesencriptado = textoDesencriptado.replaceAll(codigo[i][1], codigo[i][0]);
         }
     }
     return textoDesencriptado;
 }
-function btnCopiar(){
-    mensaje.select();
-    navigator.clipboard.writeText(mensaje.value); 
-    mensaje.value = "";
-    alert("Texto copiado")
+
+// Función para copiar el mensaje al portapapeles
+function btnCopiar() {
+    mensaje.select(); // Selecciona el texto en el elemento de mensaje
+    navigator.clipboard.writeText(mensaje.value); // Copia el texto al portapapeles
+    mensaje.value = ""; // Limpia el mensaje
+    alert("Texto copiado"); // Muestra una alerta al usuario
 }
